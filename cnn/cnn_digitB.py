03c81eea594ef2182269284b8785c9dafa745d8b
NusratAra <mubin.ara.24@gmail.com>
2017-08-14 20:46:02 +0600

    Edited with Bangla data

--- cnn/cnn_digitB.py
+++ cnn/cnn_digitB.py
@@ -3,19 +3,24 @@ import mxnet as mx
 
 
 #mnist = mx.test_utils.get_mnist()
-trainf = open("train_images.txt", "r")
-train =  trainf.read().split(',')
+import numpy
 
-trainlf = open("train_lebels.txt", "r")
-trainl =  trainlf.read().split(',')
+trainf = open("bangla_data/train_images.txt", "r")
+train =  list(trainf.read().split())
 
-testf = open("test_images.txt", "r")
-test =  testf.read().split(',')
 
-testlf = open("test_lebels.txt", "r")
-testl =  testlf.read().split(',')
+trainlf = open("bangla_data/train_labels.txt", "r")
+trainl =  list(trainlf.read().split())
 
-batch_size = 100
+print trainl
+
+testf = open("bangla_data/test_images.txt", "r")
+test =  list(testf.read().split())
+
+testlf = open("bangla_data/test_labels.txt", "r")
+testl =  list(testlf.read().split())
+
+batch_size = 784
 train_iter = mx.io.NDArrayIter(train, trainl, batch_size, shuffle=True)
 val_iter = mx.io.NDArrayIter(test, testl, batch_size)
 
@@ -52,28 +57,28 @@ lenet = mx.sym.SoftmaxOutput(data=fc2, name='softmax')
 print "softmax loss"
 
 
-# create a trainable module on GPU 0
-lenet_model = mx.mod.Module(symbol=lenet, context=mx.cpu())
-# train with the same
-lenet_model.fit(train_iter,
-                eval_data=val_iter,
-                optimizer='sgd',
-                optimizer_params={'learning_rate':0.1},
-                eval_metric='acc',
-                batch_end_callback = mx.callback.Speedometer(batch_size, 100),
-                num_epoch=1)
-print "training complete"
-
-
-
-test_iter = mx.io.NDArrayIter(test, None, batch_size)
-prob = lenet_model.predict(test_iter)
-test_iter = mx.io.NDArrayIter(test, testl, batch_size)
-# predict accuracy for lenet
-acc = mx.metric.Accuracy()
-lenet_model.score(test_iter, acc)
-print(acc)
-assert acc.get()[1] > 0.98
+# # create a trainable module on GPU 0
+# lenet_model = mx.mod.Module(symbol=lenet, context=mx.cpu())
+# # train with the same
+# lenet_model.fit(train_iter,
+#                 eval_data=val_iter,
+#                 optimizer='sgd',
+#                 optimizer_params={'learning_rate':0.1},
+#                 eval_metric='acc',
+#                 batch_end_callback = mx.callback.Speedometer(batch_size, 100),
+#                 num_epoch=1)
+# print "training complete"
+#
+#
+#
+# test_iter = mx.io.NDArrayIter(test, None, batch_size)
+# prob = lenet_model.predict(test_iter)
+# test_iter = mx.io.NDArrayIter(test, testl, batch_size)
+# # predict accuracy for lenet
+# acc = mx.metric.Accuracy()
+# lenet_model.score(test_iter, acc)
+# print(acc)
+# assert acc.get()[1] > 0.98
 trainf.close()
 trainlf.close()
 testf.close()
